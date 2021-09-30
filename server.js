07207e244284834356a4231d42939bf489986efb
gpierre004@gmail.com <gpierre004@gmail.com>
2021-09-30 18:01:50 -0400

    Posting with Postman

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyPaser = require('body-parser');
+
+/* handling all the parsing */
+app.use(bodyPaser.json());
 
 app.post('/', (req, res)=>{
   var email = req.body.email;
   var amount = req.body.amount;
-
   res.send({"amount": amount, "email": email})
 });
 app.listen(3000,()=>{
